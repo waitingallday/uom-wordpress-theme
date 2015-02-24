cf11f6e4475aa524db5332218e1b5c3e24e62f91
Jason Smith <jason@waitingallday.com>
2015-02-24 17:17:57 +1100

    hack the categories

--- wp-content/themes/uom/functions.php
+++ wp-content/themes/uom/functions.php
@@ -22,7 +22,10 @@ function single_category_link() {
   if (count($cats) > 0) {
 
     $cat = $cats[0];
-    print_r($cat['slug']);
+
+    print_r($cat);
+
+    print_r($cat[slug]);
 
     return '<p class="topic"><a href="'.get_option('home').'/category/'.$cat['slug'].'">'.ucfirst($cats['name']).'</a></p>';
   }

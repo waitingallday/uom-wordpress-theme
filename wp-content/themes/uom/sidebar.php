3ed42721febe63a0bc549fb02adac0e92a71c628
Jason Smith <jason@waitingallday.com>
2015-02-26 10:50:31 +1100

    update nav content

--- wp-content/themes/uom/sidebar.php
+++ wp-content/themes/uom/sidebar.php
@@ -5,33 +5,27 @@
  */
 ?>
 <div id="sitemap" role="navigation">
-  <div class="w">
-    <ul>
-
-			<li>
-				<?php get_search_form(); ?>
-			</li>
-
-			<li>
-			  Pages
-			  <ul>
-					<?php wp_list_pages(); ?>
-				</ul>
-			</li>
-			<li>
-			  Archives
+  <ul>
+		<?php wp_list_pages(); ?>
+		<li>
+		  Archives
+			<div class="inner">
 				<ul>
 					<?php wp_get_archives('type=monthly'); ?>
 				</ul>
-			</li>
-			<li>
-			  Categories
+			</div>
+		</li>
+		<li>
+		  Categories
+			<div class="inner">
 			  <ul>
 					<?php wp_list_categories('show_count=1'); ?>
 				</ul>
-			</li>
-			<li><?php wp_loginout(); ?></li>
-			<li><?php wp_meta(); ?></li>
-		</ul>
-	</div>
+			</div>
+		</li>
+		<li>
+			<?php get_search_form(); ?>
+		</li>
+		<li><?php wp_loginout(); ?></li>
+	</ul>
 </div>

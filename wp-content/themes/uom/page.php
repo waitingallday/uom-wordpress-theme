d05d36e05b6df79a8930720457fcd75fca05e70f
Jason Smith <jason@waitingallday.com>
2015-02-26 15:57:25 +1100

    fix content page template

--- wp-content/themes/uom/page.php
+++ wp-content/themes/uom/page.php
@@ -7,26 +7,25 @@
 get_header(); ?>
 
 <div class="page-local-history">
-  <a class="last" href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
+  <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
+	<span>/</span>
+  <a href="."><?php the_title(); ?></a>
 </div>
 
 <div role="main">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="post" id="post-<?php the_ID(); ?>">
+		<h1><?php the_title(); ?></h1>
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="post" id="post-<?php the_ID(); ?>">
-		<h2><?php the_title(); ?></h2>
-			<div class="entry">
-				<?php the_content('<p class="serif">' . __('Read the rest of this page &raquo;', 'kubrick') . '</p>'); ?>
-
-				<?php wp_link_pages(array('before' => '<p><strong>' . __('Pages:', 'kubrick') . '</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-
-			</div>
+		<div class="entry">
+			<?php the_content(); ?>
 		</div>
-		<?php endwhile; endif; ?>
-	<?php edit_post_link(__('Edit this entry.', 'kubrick'), '<p>', '</p>'); ?>
+	</div>
+
+	<?php endwhile; endif; ?>
+	<?php edit_post_link('Edit'); ?>
 
 	<?php comments_template(); ?>
-
 </div>
 
 <?php get_footer(); ?>

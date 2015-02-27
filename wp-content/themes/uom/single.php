44c6c9f530be082b297f081c540e130d531d0399
Jason Smith <jason@waitingallday.com>
2015-02-27 13:21:42 +1100

    fix alignment

--- wp-content/themes/uom/single.php
+++ wp-content/themes/uom/single.php
@@ -21,7 +21,7 @@ if (count($banner) > 0)
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?>"<?php echo $banner ?>>
 		<header>
-			<div class="mid-align">
+			<div class="bottom-align">
 				<h1><?php the_title(); ?></h1>
 			</div>
 		</header>

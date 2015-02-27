316181c0e84f602c7c1bfd74c2e80e517cce559c
Jason Smith <jason@waitingallday.com>
2015-02-27 14:41:23 +1100

    refine aside

--- wp-content/themes/uom/single.php
+++ wp-content/themes/uom/single.php
@@ -20,47 +20,21 @@ if (count($banner) > 0)
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<?php if (count($banner) > 0): ?>
 	<header class="banner"<?php echo $banner ?>></header>
-	<?php endif; ?>
 	<article class="news" id="post-<?php the_ID(); ?>">
+	<?php else: ?>
+	<article class="news noheader" id="post-<?php the_ID(); ?>">
+	<?php endif; ?>
 		<div class="entry article">
 			<h1><?php the_title(); ?></h1>
 
 			<?php the_content(); ?>
 
-			<p class="postmetadata alt">
-				<small>
-				<?php /* This is commented, because it requires a little adjusting sometimes.
-					You'll need to download this plugin, and follow the instructions:
-					http://binarybonsai.com/wordpress/time-since/ */
-					/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); $time_since = sprintf(__('%s ago', 'kubrick'), time_since($entry_datetime)); */ ?>
-				<?php printf(__('This entry was posted on %1$s at %2$s and is filed under %3$s.', 'kubrick'), get_the_time(__('l, F jS, Y', 'kubrick')), get_the_time(), get_the_category_list(', ')); ?>
-				<?php printf(__("You can follow any responses to this entry through the <a href='%s'>RSS 2.0</a> feed.", "kubrick"), get_post_comments_feed_link()); ?>
-
-				<?php if ( comments_open() && pings_open() ) {
-					// Both Comments and Pings are open ?>
-					<?php printf(__('You can <a href="#respond">leave a response</a>, or <a href="%s" rel="trackback">trackback</a> from your own site.', 'kubrick'), get_trackback_url()); ?>
-
-				<?php } elseif ( !comments_open() && pings_open() ) {
-					// Only Pings are Open ?>
-					<?php printf(__('Responses are currently closed, but you can <a href="%s" rel="trackback">trackback</a> from your own site.', 'kubrick'), get_trackback_url()); ?>
-
-				<?php } elseif ( comments_open() && !pings_open() ) {
-					// Comments are open, Pings are not ?>
-					<?php _e('You can skip to the end and leave a response. Pinging is currently not allowed.', 'kubrick'); ?>
-
-				<?php } elseif ( !comments_open() && !pings_open() ) {
-					// Neither Comments, nor Pings are open ?>
-					<?php _e('Both comments and pings are currently closed.', 'kubrick'); ?>
-
-				<?php } edit_post_link('Edit'); ?>
-				</small>
-			</p>
-
+      <?php edit_post_link('Edit'); ?>
 		</div>
 
 		<aside>
       <div>
-        <time datetime="2014-09-22">22 September 2014</time>
+        <time datetime="<?php echo get_the_time('Y-m-j') ?>2014-09-22"><?php echo get_the_time('j F Y') ?></time>
       </div>
       <div>
 	      <p>
@@ -69,12 +43,6 @@ if (count($banner) > 0)
 	      <ul>
 	        <li>
 	          <a href="#">Sustainability</a>
-	        </li>
-	        <li>
-	          <a href="#">Tim Flannery</a>
-	        </li>
-	        <li>
-	          <a href="#">Melbourne Sustainable Society Institute</a>
 	        </li>
 	      </ul>
 	    </div>

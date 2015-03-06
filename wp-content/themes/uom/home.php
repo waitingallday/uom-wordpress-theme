d45dccea971646d5c5606c8e4f46e24ec75ad674
Jason Smith <jason@waitingallday.com>
2015-03-06 12:04:57 +1100

    add homepage template

---
+++ wp-content/themes/uom/home.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage uom_theme
+ */
+
+/*
+Template Name: Homepage
+*/
+get_header(); ?>
+
+<div class="floating"></div>
+<div role="main">
+  <header class="banner" style="background-image:url(<?php header_image(); ?>)">
+    <div class="mid-align">
+      <h1><?php bloginfo('name'); ?></h1>
+      <p><?php bloginfo('description'); ?></p>
+    </div>
+  </header>
+
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <div class="post" id="post-<?php the_ID(); ?>">
+    <div class="entry">
+      <h1><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+    </div>
+  </div>
+
+  <?php endwhile; endif; ?>
+  <?php edit_post_link('Edit'); ?>
+</div>
+
+<?php get_footer(); ?>

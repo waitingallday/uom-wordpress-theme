7a01f248039f29907833047f9764a8807349e849
Jason Smith <jason@waitingallday.com>
2015-03-06 12:09:25 +1100

    debug

--- wp-content/themes/uom/functions.php
+++ wp-content/themes/uom/functions.php
@@ -64,6 +64,7 @@ function render_pages_list() {
   // 2 level parent-child menu
   foreach ( (array) $pages as $page )
     if (0 === $page->post_parent && "publish" === $page->post_status) {
+      print_r($page);
       echo '<li><a href="'.get_option('home').'/'.$page->post_name.'">'.$page->post_title.'</a>';
 
       $c = 0;
--- wp-content/themes/uom/home.php
+++ wp-content/themes/uom/home.php
@@ -21,7 +21,6 @@ get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <div class="post" id="post-<?php the_ID(); ?>">
     <div class="entry">
-      <h1><?php the_title(); ?></h1>
       <?php the_content(); ?>
     </div>
   </div>

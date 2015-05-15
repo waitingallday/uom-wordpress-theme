84bc1700b0e71462cb1b59184a673dbcc0ecd732
Jason Smith <jason@waitingallday.com>
2015-05-15 16:12:29 +1000

    style meta

--- wp-content/themes/uom/home.php
+++ wp-content/themes/uom/home.php
@@ -26,9 +26,9 @@ get_header(); ?>
         <?php single_category_link(); ?>
         <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
         <?php the_content('Read more'); ?>
-        <p>
+        <p class="meta">
           Posted <time datetime="<?php echo get_the_time('Y-m-j') ?>"><?php echo get_the_time('j F Y') ?></time>
-          <?php comments_popup_link(__('No Comments &#187;', 'kubrick'), __('1 Comment &#187;', 'kubrick'), __('% Comments &#187;', 'kubrick'), '', __('Comments Closed', 'kubrick') ); ?>
+          <?php comments_popup_link(__('(No Comments)', 'kubrick'), __('(1 Comment)', 'kubrick'), __('(% Comments)', 'kubrick'), '', __('(Comments Closed)', 'kubrick') ); ?>
           <?php edit_post_link('Edit'); ?>
         </p>
       </article>
